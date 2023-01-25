f3c3f48c630b030cb86019a76e4c9a76ee3cc48c
KoushikDuttaHih7 <koushik.dutta@hih7.in>
2023-01-25 18:04:48 +0530

    views changed

--- app.js
+++ app.js
@@ -22,7 +22,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, "public")));
 
 app.use((req, res, next) => {
-  User.findById("5baa2528563f16379fc8a610")
+  User.findById("")
     .then((user) => {
       req.user = new User(user.name, user.email, user.cart, user._id);
       next();
