e5735eebbfdd6367e177e45b25c4afa232113cf8
KoushikDuttaHih7 <koushik.dutta@hih7.in>
2023-01-31 17:06:15 +0530

    updated database & User name

--- app.js
+++ app.js
@@ -22,7 +22,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, "public")));
 
 app.use((req, res, next) => {
-  User.findById("63d4d113bbf325e7ebdb6c3e")
+  User.findById("63d8fc01beb6de9bea159fe0")
     .then((user) => {
       req.user = new User(user.name, user.email, user.cart, user._id);
       next();
